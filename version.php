e3c0d00d67e5aa5e88a3a1daeb53231e80429416
rmbinder <r-m-b@gmx.net>
2019-02-04 13:27:29 +0100

    update version

--- version.php
+++ version.php
@@ -3,12 +3,12 @@
  ***********************************************************************************************
  * Versionsdatei fuer das Admidio-Plugin FormFiller
  *
- * @copyright 2004-2018 The Admidio Team
+ * @copyright 2004-2019 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  *
  ***********************************************************************************************
  */
 
-$plugin_version = '2.3.2';
-$plugin_stand   = '30.10.2018';
+$plugin_version = '2.3.3';
+$plugin_stand   = '04.02.2019';
