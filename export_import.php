ca5918583996296daac7cf32773f112c71ff1865
ELITEBOOK840G4\rmb <r-m-b@gmx.net>
2025-04-07 18:26:19 +0200

    namespace used, #82

--- export_import.php
+++ export_import.php
@@ -16,6 +16,9 @@
  ***********************************************************************************************
  */
 
+use Admidio\Infrastructure\Utils\FileSystemUtils;
+use Admidio\Infrastructure\Utils\SecurityUtils;
+
 require_once(__DIR__ . '/../../adm_program/system/common.php');
 require_once(__DIR__ . '/common_function.php');
 require_once(__DIR__ . '/classes/configtable.php');
